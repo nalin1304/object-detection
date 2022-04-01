387e6ecba5fe2bfd78c4c62c50d45107842262ed
nalin1304 <76690140+nalin1304@users.noreply.github.com>
2022-04-01 15:08:42 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,19 @@
+function one(){
+    window.location="one.html" ;
+
+}
+function two(){
+    window.location="two.html" ;
+    
+}function three(){
+    window.location="three.html" ;
+    
+}
+function four(){
+    window.location="four.html" ;
+    
+}
+function  five (){
+    window.location="five.html" ;
+    
+}
---
+++ three.js
@@ -0,0 +1,46 @@
+Status="";
+objects= [];
+function redirect(){
+    window.location="home.html";
+}
+
+
+function setup(){
+canvas=createCanvas(650,400);
+canvas.center();
+Object_detector= ml5.objectDetector('cocossd',modelLoaded);
+document.getElementById("status").innerHTML="Status: Detecting object";
+}
+function preload(){
+img=loadImage("ani.jpeg");
+}
+function draw(){
+image(img,0,0,650,400);
+if(Status!=""){
+    for(i=0; i<objects.length; i++){
+        document.getElementById("status").innerHTML="Status: object detected";
+        fill("Red");
+        confidence= Math.round(objects[i].confidence*100)+" % ";
+        text(objects[i].label+" "+confidence,objects[i].x,objects[i].y);
+        noFill();
+        stroke("Red");
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
+    }
+}
+
+}
+function modelLoaded(){
+console.log("model is loaded");
+Status=true;
+Object_detector.detect(img,gotResults);
+}
+function gotResults(error,results){
+if(error){
+    console.log(error)
+    
+}
+else{
+    console.log(results);
+    objects= results;
+}
+}
